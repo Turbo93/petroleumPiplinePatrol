a1079921d2e34f3cdf37be4f96b90cfd1697bb80
ghstaytrue <610559612@qq.com>
2015-07-08 21:49:53 +0800

    first

--- gen/com/etc/syww/petroleumPiplinePatrol/R.java
+++ gen/com/etc/syww/petroleumPiplinePatrol/R.java
@@ -21,10 +21,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_imageprocessing=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_map=0x7f020002;
+        public static final int ic_setings=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int ibt_camera=0x7f080001;
+        public static final int ibt_imageprocessing=0x7f080003;
+        public static final int ibt_map=0x7f080002;
+        public static final int ibt_settings=0x7f080004;
+        public static final int ll_start=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/etc/syww/petroleumPiplinePatrol/MainActivity.java
+++ src/com/etc/syww/petroleumPiplinePatrol/MainActivity.java
@@ -3,6 +3,9 @@ package com.etc.syww.petroleumPiplinePatrol;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +13,49 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ibt_camera = (ImageButton) findViewById(R.id.ibt_camera);
+		ibt_map = (ImageButton) findViewById(R.id.ibt_map);
+		ibt_imageprocessing = (ImageButton) findViewById(R.id.ibt_imageprocessing);
+		ibt_settings = (ImageButton) findViewById(R.id.ibt_settings);
+		ibt_camera.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				
+			}});
+		ibt_map.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		ibt_imageprocessing.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		ibt_settings.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		System.out.println("aaaaaaaa");
 		return true;
 	}
+	private ImageButton ibt_camera;
+	private ImageButton ibt_map;
+	private ImageButton ibt_imageprocessing;
+	private ImageButton ibt_settings;
 
 }
